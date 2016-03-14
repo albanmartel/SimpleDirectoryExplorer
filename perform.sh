98d8bcd179414302d1a56a7dcfe5e638a92611ab
Alban MARTEL <albanmartel@users.noreply.github.com>
2016-03-14 11:24:51 +0100

    Create perform.sh
    To start the python modulates directories exploration

---
+++ perform.sh
@@ -0,0 +1,7 @@
+# Par ''Alban MARTEL''
+# Courriel : albanmartel(POINT)developpeur(AT)gmail(POINT)com
+
+#To start the python modulates directories exploration
+
+# !/bin/bash
+python3  ExplorateurRepertoire_Control.py

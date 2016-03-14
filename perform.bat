6d85fc2b3271bd9991036cbbf5df63837b5c0d11
Alban MARTEL <albanmartel@users.noreply.github.com>
2016-03-14 11:39:34 +0100

    Update perform.bat

--- perform.bat
+++ perform.bat
@@ -1,1 +1,6 @@
-::
+::Par ''Alban MARTEL''
+::Courriel : albanmartel(POINT)developpeur(AT)gmail(POINT)com
+
+::To start the python modulates directories exploration
+
+C:\Python34\python ExplorateurRepertoire_Control.py
